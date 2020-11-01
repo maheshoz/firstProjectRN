b54f917b13006137444110dd59fe3f606cba0802
mahehsoz <maheshcn31@gmail.com>
2020-11-01 20:19:07 +0530

    add touchableOpacity

--- App.js
+++ App.js
@@ -1,38 +1,38 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
 
 export default function App() {
   const [people, setPeople] = useState([
-    { name: "shaun", key: "1" },
-    { name: "John", key: "2" },
-    { name: "Sam", key: "3" },
-    { name: "Mario", key: "4" },
-    { name: "Fury", key: "5" },
-    { name: "Hulk", key: "6" },
-    { name: "Thor", key: "7" },
+    { name: 'shaun', id: '1' },
+    { name: 'yoshi', id: '2' },
+    { name: 'mario', id: '3' },
+    { name: 'luigi', id: '4' },
+    { name: 'peach', id: '5' },
+    { name: 'toad', id: '6' },
+    { name: 'bowser', id: '7' },
   ]);
+
+  const pressHandler = (id) => {
+    console.log(id);
+    setPeople((prevPeople) => {
+      return prevPeople.filter(person => person.id  != id);
+    });
+  };
 
   return (
     <View style={styles.container}>
-      <FlatList
+
+      <FlatList 
         numColumns={2}
-        keyExtractor = { (item)=> item.id}
-        data={people}
-        renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
+        keyExtractor={(item) => item.id} 
+        data={people} 
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => pressHandler(item.id)}>
+            <Text style={styles.item}>{item.name}</Text>
+          </TouchableOpacity>
+        )}
       />
 
-      {/* <ScrollView>
-     {people.map( item=>{
-       return(
-         <View key={item.key}>
-           <Text style={styles.item}>
-             {item.name}
-          </Text>
-         </View>
-       )
-     }
-     )}
-     </ScrollView> */}
     </View>
   );
 }
@@ -40,17 +40,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     paddingTop: 40,
     paddingHorizontal: 20,
-    // alignItems: "center",
-    // justifyContent: "center",
+    backgroundColor: '#fff',
   },
   item: {
+    flex: 1,
+    marginHorizontal: 10,
     marginTop: 24,
     padding: 30,
-    backgroundColor: "pink",
+    backgroundColor: 'pink',
     fontSize: 24,
-    marginHorizontal: 8,
   },
 });
